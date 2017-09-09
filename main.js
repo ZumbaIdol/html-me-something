ed50ec947fbc90e53784df6f526eed175bc0bd22
david.a.foote@gmail.com <david.a.foote@gmail.com>
2017-09-08 21:56:30 -0500

    Tried a bunch of stuff, didn't work, reverted back

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 main.js
-var red = [0, 100, 63];
+var red = [359, 100, 29.8];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "How Comics Help to Shape My Intelligence";
-letterColors = [red, orange, green, blue];
+var myName = "David A. Foote";
+var letterColors = [red, orange, greem, blue];
+var bubbleShape = "circle";
+
 drawName(myName, letterColors);
-bounceBubbles()
+bounceBubbles();
 
