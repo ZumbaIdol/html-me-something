248f353f754c51ebb23588e49dfc2c27b6b75f9d
david.a.foote@gmail.com <david.a.foote@gmail.com>
2017-09-12 22:42:45 -0500

    Worked on JavaScript effect 9/12/17

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-var red = [359, 100, 29.8];
+var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
 var myName = "David A. Foote";
-var letterColors = [red, orange, purple, blue];
-var bubbleShape = "circle";
+letterColors = [red, orange, blue, purple];
+
+
+
 
 drawName(myName, letterColors);
 bounceBubbles();
